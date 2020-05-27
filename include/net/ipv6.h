84939c5dea48a419b80acdae917a25c0550f22c5
Sabrin Dubroca <sd@queasysnail.net>
2020-05-27 10:21:21 -0400

    net: ipv6: add net argument to ip6_dst_lookup_flow

--- include/net/ipv6.h
+++ include/net/ipv6.h
@@ -851,7 +851,7 @@ static inline struct sk_buff *ip6_finish_skb(struct sock *sk)
 			      &inet6_sk(sk)->cork);
 }
 
-int ip6_dst_lookup(struct net *net, struct sock *sk, struct dst_entry **dst,
+struct dst_entry *ip6_dst_lookup_flow(struct net *net, const struct sock *sk, struct flowi6 *fl6,
 		   struct flowi6 *fl6);
 struct dst_entry *ip6_dst_lookup_flow(const struct sock *sk, struct flowi6 *fl6,
 				      const struct in6_addr *final_dst);
